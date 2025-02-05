39122cfb26a339137a86471d0c9b3e40d2e1bf68
rkaur2612 <155934341+rkaur2612@users.noreply.github.com>
2025-02-04 21:26:38 -0800

    Update Valid Anagram.py

--- NeetCode150/Arrays & Hashing/Valid Anagram.py
+++ NeetCode150/Arrays & Hashing/Valid Anagram.py
@@ -1,1 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
 
+        dict_s = {}
+        for c in s: #O(n)
+            if c not in dict_s: #O(1)
+                dict_s[c] = 1 #O(1)
+            else:
+                dict_s[c] += 1
+        
+        for c in t: #O(n)
+            if c not in dict_s:#O(1)
+                return False
+            else:
+                dict_s[c] -= 1
+        
+        for k in dict_s:
+            if dict_s[k] != 0:
+                return False
+        return True
+
+        #Space - O(n)
+        #Time - O(n)
