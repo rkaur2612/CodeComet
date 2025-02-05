9af36cb828e62d60c7395850359cbc4fc40c45a7
rkaur2612 <155934341+rkaur2612@users.noreply.github.com>
2025-02-04 21:25:38 -0800

    Update Contains Duplicate.py

--- NeetCode150/Arrays & Hashing/Contains Duplicate.py
+++ NeetCode150/Arrays & Hashing/Contains Duplicate.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        dup_dict = {}
+        for num in nums: #O(n)
+            if num not in dup_dict: #O(1)
+                dup_dict[num] = 1
+            else:
+                return True
+        return False
+         
+#Time complexity - O(n)
+#Space complecity - O(n)
